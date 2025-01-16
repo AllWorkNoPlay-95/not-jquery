19066d60d1c310134d07ec74fc4842e924fef772
Samuele Mancuso <s.mancuso95@gmail.com>
2025-01-16 10:10:01 +0100

    functions.php

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package not-jquery
+ * @author Samuele Mancuso
+ * @link https://github.com/AllWorkNoPlay-95/not-jquery
+ * @description A lightweight JavaScript library that emulates essential jQuery functions, designed for WordPress themes and plugins without relying on the full jQuery library.
+ */
+
+namespace mncs\not_jquery;
+function enqueue_fe() {
+    wp_enqueue_script(
+        'mncs-not-jquery', // Handle
+        get_template_directory_uri() . '/js/not-jquery.min.js', // Path to the script
+        [], // Dependencies (none required)
+        '1.0.0', // Version number
+        true // Load script in the footer
+    );
+}
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_fe');
