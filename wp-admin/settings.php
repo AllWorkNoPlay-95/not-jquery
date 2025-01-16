54335146fd7a2ad2ccfaf8921b4bffbd6eebd1c5
Samuele Mancuso <s.mancuso95@gmail.com>
2025-01-16 13:15:26 +0100

    reformat and fix wrong use of array as capabilities param

--- wp-admin/settings.php
+++ wp-admin/settings.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace mncs\not_jquery\admin;
 defined("ABSPATH") or die();
+
 use const mncs\not_jquery\OPTION_GROUP;
+
 function register_settings(): void
 {
     register_setting(OPTION_GROUP, OPTION_GROUP . "_dequeue_jquery", [
@@ -14,24 +17,24 @@ add_action("admin_init", __NAMESPACE__ . '\register_settings');
 function print_settings_page(): void
 {
     ?>
-    <div class="wrap">
-        <h1><?php echo OPTION_GROUP; ?></h1>
-        <form action="options.php" method="POST">
-            <?php settings_fields(OPTION_GROUP); ?>
-            <?php do_settings_sections(OPTION_GROUP); ?>
-            <h2>Not jQuery</h2>
-            <input name="<?php echo OPTION_GROUP .
-                "_dequeue_jquery"; ?>" type="checkbox"
-                   value="1" <?php checked(
-                       "1",
-                       get_option(OPTION_GROUP . "_dequeue_jquery")
-                   ); ?> />
-            <label for="<?php echo OPTION_GROUP .
-                "_dequeue_jquery"; ?>">Dequeue jQuery?</label>
-            <br/>
-            <?php submit_button(); ?>
-        </form>
-    </div>
+  <div class="wrap">
+    <h1><?php echo OPTION_GROUP; ?></h1>
+    <form action="options.php" method="POST">
+        <?php settings_fields(OPTION_GROUP); ?>
+        <?php do_settings_sections(OPTION_GROUP); ?>
+      <h2>Not jQuery</h2>
+      <input name="<?php echo OPTION_GROUP .
+          "_dequeue_jquery"; ?>" type="checkbox"
+             value="1" <?php checked(
+                 "1",
+                 get_option(OPTION_GROUP . "_dequeue_jquery")
+             ); ?> />
+      <label for="<?php echo OPTION_GROUP .
+          "_dequeue_jquery"; ?>">Dequeue jQuery?</label>
+      <br />
+        <?php submit_button(); ?>
+    </form>
+  </div>
     <?php
 }
 
@@ -40,9 +43,10 @@ function add_menu(): void
     add_options_page(
         "Not jQuery",
         "Not jQuery",
-        ["administrator"],
+        "administrator",
         OPTION_GROUP,
         __NAMESPACE__ . "\print_settings_page"
     );
 }
+
 add_action("admin_menu", __NAMESPACE__ . "\add_menu");
